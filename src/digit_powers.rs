b3ba745cbeaa4499ba569bd4dcf49a4cb5b71f0e
Christian Kischy Haege <kischy@gmx.de>
2021-10-04 22:56:30 +0200

    Some refactoring

--- src/digit_powers.rs
+++ src/digit_powers.rs
@@ -2,59 +2,59 @@ mod digits;
 use digits::get_digits;
 
 pub struct DigitPowers {
-    exponent: u32,
-    digit_power_numbers: Option<Vec<u128>>,
+    exp: u32,
+    digit_power_nums: Option<Vec<u128>>,
 }
 
 impl DigitPowers {
-    pub fn new(exponent: u32) -> DigitPowers {
+    pub fn new(exp: u32) -> DigitPowers {
         DigitPowers {
-            exponent,
-            digit_power_numbers: None,
+            exp,
+            digit_power_nums: None,
         }
     }
 
-    pub fn get_numbers(&mut self) -> Vec<u128> {
-        if let None = self.digit_power_numbers {
-            self.calc_numbers();
+    pub fn get_nums(&mut self) -> Vec<u128> {
+        if let None = self.digit_power_nums {
+            self.calc_nums();
         }
-        return self.digit_power_numbers.clone().unwrap();
+        return self.digit_power_nums.clone().unwrap();
     }  
 }
 
 impl DigitPowers {
     fn calc_boundaries(&self) -> (u128, u128) {
         let mut i = 1;
-        let mut upper: u128 = 9_u128.pow(self.exponent);
+        let mut upper: u128 = 9_u128.pow(self.exp);
         let mut left_num: u128 = 9;
 
         while left_num < upper {
             i += 1;
-            upper = i * 9_u128.pow(self.exponent);
+            upper = i * 9_u128.pow(self.exp);
             left_num = left_num * 10 + 9;
         }
 
-        (2_u128.pow(self.exponent), upper)
+        (2_u128.pow(self.exp), upper)
     }
 
-    fn calc_digit_power(&self, digit: u128) -> u128 {
+    fn calc_single_digit_power(&self, digit: u128) -> u128 {
         let mut  dp = 0;
         for digit in get_digits(digit) {
-            dp += (digit as u128).pow(self.exponent);
+            dp += (digit as u128).pow(self.exp);
         }
         dp
     }
 
-    fn calc_numbers(&mut self) {
+    fn calc_nums(&mut self) {
         let mut nums: Vec<u128> = Vec::new();
         let (lower, upper) = self.calc_boundaries();
 
         for num in lower..=upper {
-            if num == self.calc_digit_power(num) {
+            if num == self.calc_single_digit_power(num) {
                 nums.push(num);
             }
         }
-        self.digit_power_numbers = Some(nums);
+        self.digit_power_nums = Some(nums);
     }
 }
 
@@ -64,6 +64,6 @@ mod tests {
     #[test]
     fn get_digit_powers_of_4() {
         let mut digit_powers = DigitPowers::new(4);
-        assert_eq!(digit_powers.get_numbers().iter().sum::<u128>(), 19316);
+        assert_eq!(digit_powers.get_nums().iter().sum::<u128>(), 19316);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,8 @@ use digit_powers::DigitPowers;
 fn main() {
     println!("Calculation started");
     let mut dps = DigitPowers::new(5);
-    println!("{:?}", dps.get_numbers());
-    let answer_p30: u128 = dps.get_numbers().iter().sum();
+    println!("{:?}", dps.get_nums());
+    let answer_p30: u128 = dps.get_nums().iter().sum();
 
     println!(
         "The answer to problem 30 of project Euler is {}.",
